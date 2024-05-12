2916686d33ba3890bcc083e8640170d97f762d88
Nour Elfangary <noureldinesamehelfangary@gmail.com>
2024-05-12 17:38:58 +0100

    added glow effects

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './styles/App.css';
 
 export function App() {
 
-  return (
-      <div className={'content'}>
-          <div className={'main'}>
-              <h1>Nour Elfangary</h1>
-          </div>
-          <div className={'links'}>
+    interface Position {
+        x: number
+        y: number
+    }
 
-          </div>
-      </div>
+    useEffect(() => {
+        const syncPointer = (props: Position) => {
+            document.documentElement.style.setProperty('--x', props.x.toFixed(2))
+            document.documentElement.style.setProperty(
+                '--xp',
+                (props.x / window.innerWidth).toFixed(2)
+            )
+            document.documentElement.style.setProperty('--y', props.y.toFixed(2))
+            document.documentElement.style.setProperty(
+                '--yp',
+                (props.y / window.innerHeight).toFixed(2)
+            )
+        }
+        document.body.addEventListener('pointermove', syncPointer)
+    })
+
+  return (
+      <main>
+          <article data-glow>
+              <div data-glow></div>
+          </article>
+          <article data-glow>
+              <div data-glow></div>
+          </article>
+      </main>
   );
 }
