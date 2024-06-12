64204f7f541fc54d9046d3fdeffe65c00901c404
Nour Elfangary <noureldinesamehelfangary@gmail.com>
2024-06-12 20:29:24 +0400

    added, files, links, bio, and cursor interaction features.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 import './styles/App.css';
+import {FaGithub, FaInstagram, FaLinkedin} from "react-icons/fa";
+import {IoDocumentTextOutline} from "react-icons/io5";
+import CV from "./CV.pdf";
+import VanillaTilt from "vanilla-tilt";
+import {motion} from "framer-motion";
+
+function Tilt(props: any) {
+    const { options, ...rest } = props;
+    const tilt = useRef(null);
+
+    useEffect(() => {
+        // @ts-ignore
+        VanillaTilt.init(tilt.current, options);
+    }, [options]);
+
+    return <main ref={tilt} {...rest} />;
+}
 
 export function App() {
 
     interface Position {
-        x: number
+        x: number,
         y: number
     }
+
+    const tiltOptions = {
+        scale: 1.1,
+        speed: 5000,
+        max: 10
+    };
 
     useEffect(() => {
         const syncPointer = (props: Position) => {
@@ -26,7 +49,7 @@ export function App() {
 
   return (
       <>
-          <main>
+          <Tilt className="box" options={tiltOptions}>
               <div className={'main'}>
                   <article data-glow>
                       <h1>Nour Elfangary</h1>
@@ -34,29 +57,40 @@ export function App() {
                   <article data-glow>
                       <h3>About Me</h3>
                       <hr/>
-                      <p> Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                          Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero
-                          sit
-                          amet
-                          quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. </p>
+                      <p> Born and raised internationally, I'm a young professional software developer currently
+                          studying Computer Science at the University of Glasgow. As I approach my final year of
+                          studies, I look forward
+                          to entering the work force and watch myself continue to learn and grow. </p>
                       <hr/>
                   </article>
               </div>
               <div className={'side'}>
-                  <article data-glow>
-                      CV
-                  </article>
-                  <article data-glow>
-                      github
-                  </article>
-                  <article data-glow>
-                      linkedin
-                  </article>
-                  <article data-glow>
-                      instagram
-                  </article>
+                  <motion.a href={CV} target="_blank" whileHover={{scale: 1.1}} whileTap={{scale: 0.9}}>
+                      <article data-glow>
+                          <IoDocumentTextOutline/>
+                          CV
+                      </article>
+                  </motion.a>
+                  <motion.a href="https://github.com/nour2063" target="_blank" whileHover={{scale: 1.1}} whileTap={{scale: 0.9}}>
+                      <article data-glow>
+                          <FaGithub/>
+                          GitHub
+                      </article>
+                  </motion.a>
+                  <motion.a href="https://www.linkedin.com/in/nour-elfangary/" target="_blank" whileHover={{scale: 1.1}} whileTap={{scale: 0.9}}>
+                      <article data-glow>
+                          <FaLinkedin/>
+                          LinkedIn
+                      </article>
+                  </motion.a>
+                  <motion.a href="https://www.instagram.com/noureldineee_/" target="_blank" whileHover={{scale: 1.1}} whileTap={{scale: 0.9}}>
+                      <article data-glow>
+                          <FaInstagram/>
+                          Instagram
+                      </article>
+                  </motion.a>
               </div>
-          </main>
+          </Tilt>
       </>
   );
 }
