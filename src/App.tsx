96f828858eb070df366d99e9872a7e5f5fe64491
Nour Elfangary <noureldinesamehelfangary@gmail.com>
2025-05-19 12:48:35 +0100

    fixed background reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,15 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './styles/App.css'
-import {Sidebar} from "./components/sidebar";
 import Background from "./components/background";
-import photo from "./grass.jpg"
-import Tilt from "react-parallax-tilt";
-import {About} from "./components/about";
-import {Links} from "./components/links";
-import {IoIosArrowBack} from "react-icons/io";
+import {Block} from "./components/block";
+
 
 export function App() {
-    const [active, setActive] = useState("about");
-
-    const handleBack = () => {
-        setActive("about");
-    };
 
     return (
         <div className="App">
             <Background/>
-            <div className="block">
-                <header>
-                    <h1>Nour Elfangary</h1>
-                    {active === "about" && (
-                        <Tilt className={"image"}>
-                            <img src={photo} alt=""/>
-                        </Tilt>
-                    )}
-                </header>
-                <main>
-                    <Sidebar setActive={setActive}/>
-                    <div className={"content"}>
-                        {active !== "about" && (
-                            <div className={"button"} id={"back"} onClick={handleBack}>
-                                <IoIosArrowBack className={"icon"}/>
-                                back
-                            </div>
-                        )}
-                        <About className={active === "about" ? "show" : "hide"}/>
-                        <Links className={active === "links" ? "show" : "hide"}/>
-                        {/*<Projects className={active === "projects" ? "show" : "hide"}/>*/}
-                        {/*<Photos className={active === "photos" ? "show" : "hide"}/>*/}
-                        {/*<Friends className={active === "friends" ? "show" : "hide"}/>*/}
-                    </div>
-                    <div className="mobileBuffer" />
-                </main>
-            </div>
+            <Block/>
         </div>
     );
 }
---
+++ src/components/block.tsx
@@ -0,0 +1,46 @@
+import Tilt from "react-parallax-tilt";
+import {About} from "./about";
+import {Links} from "./links";
+import {IoIosArrowBack} from "react-icons/io";
+import photo from "../grass.jpg";
+import {Sidebar} from "./sidebar";
+import React, {useState} from "react";
+
+export function Block() {
+
+    const [active, setActive] = useState("about");
+
+    const handleBack = () => {
+        setActive("about");
+    };
+
+    return (
+        <div className="block">
+            <header>
+                <h1>Nour Elfangary</h1>
+                {active === "about" && (
+                    <Tilt className={"image"}>
+                        <img src={photo} alt=""/>
+                    </Tilt>
+                )}
+            </header>
+            <main>
+                <Sidebar setActive={setActive}/>
+                <div className={"content"}>
+                    {active !== "about" && (
+                        <div className={"button"} id={"back"} onClick={handleBack}>
+                            <IoIosArrowBack className={"icon"}/>
+                            back
+                        </div>
+                    )}
+                    <About className={active === "about" ? "show" : "hide"}/>
+                    <Links className={active === "links" ? "show" : "hide"}/>
+                    {/*<Projects className={active === "projects" ? "show" : "hide"}/>*/}
+                    {/*<Photos className={active === "photos" ? "show" : "hide"}/>*/}
+                    {/*<Friends className={active === "friends" ? "show" : "hide"}/>*/}
+                </div>
+                <div className="mobileBuffer"/>
+            </main>
+        </div>
+    );
+}
